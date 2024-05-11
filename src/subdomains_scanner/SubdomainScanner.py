48991149f4407f97ffb1834a8537243786644161
Jonathan-404 <yt.gamerstop.yt@gmail.com>
2024-05-11 13:54:26 +0300

    time-taken feture removed and set threads to 50 in gobuster.

--- src/subdomains_scanner/SubdomainScanner.py
+++ src/subdomains_scanner/SubdomainScanner.py
@@ -15,24 +15,13 @@ class SubdomainScanner:
     def start_subdomain_scan(self):
         try:
             # Build the GoBuster command for DNS subdomain enumeration
-            command = ['gobuster', 'dns', '-d', self.domain]
+            command = ['gobuster', 'dns', '-d', self.domain, '-t', '50']
             if self.wordlist:
                 command.extend(['-w', self.wordlist])
             if self.output_file:
                 command.extend(['-o', self.output_file])
 
-            # Run GoBuster command to scan subdomains
-            before = datetime.datetime.now()
             result = subprocess.run(command, capture_output=True, text=True)
-            after = datetime.datetime.now()
 
-            # Check if the command was successful
-            if result.returncode == 0:
-                return after - before
-            else:
-                # If the command failed, print the error message
-                print("Error:", result.stderr)
-                return []
         except FileNotFoundError:
             print("GoBuster tool not found. Make sure it's installed and in your PATH.")
-            return []
