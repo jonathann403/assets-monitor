29c5420a82dcace10454ab865ce9477736f5afb2
Jonathan-404 <yt.gamerstop.yt@gmail.com>
2024-05-11 15:07:21 +0300

    finally fixexd all errors that are process related.

--- run.py
+++ run.py
@@ -1,35 +1,49 @@
-import os
 from app import create_app
 from src.subdomains_scanner import SubdomainScanner
-from multiprocessing import Process, Pipe, set_start_method
+from multiprocessing import Process, set_start_method
+import subprocess
 
 domain = "ynet.co.il"
 
-def run_scanner(pipe_handle):
+
+def run_scanner():
     try:
         scanner = SubdomainScanner(domain, "wordlists/subdomains/httparchive_subdomains_2024_04_28.txt")
         scanner.start_subdomain_scan()
+
     except Exception as e:
         print(e)
-    finally:
-        pipe_handle.close()  # Close the pipe handle after finishing
+
 
 def run_flask_app():
     app = create_app()
     app.run(debug=True)
 
-if __name__ == '__main__':
 
-    parent_conn, child_conn = Pipe()
+def remove_gobuster_process():
+    try:
+        gobuster_procces = str(subprocess.check_output(["pgrep", "gobuster"]).decode()).strip()
+        subprocess.run(["kill", gobuster_procces])
+    except Exception as e:
+        pass
 
-    scanner_process = Process(target=run_scanner, args=(child_conn,))
+def main():
+    try:
+        set_start_method('fork')
+    except RuntimeError:
+        pass  # 'fork' method is not available
+
+    scanner_process = Process(target=run_scanner)
     flask_app_process = Process(target=run_flask_app)
 
-    scanner_process.start()
     flask_app_process.start()
+    scanner_process.start()
+
+    remove_gobuster_process() # because it creates two
 
     scanner_process.join()
     flask_app_process.join()
 
-    # Close the parent connection after both processes are done
-    parent_conn.close()
+
+if __name__ == '__main__':
+    main()
